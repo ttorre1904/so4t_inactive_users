8bb91bebee4c7838b61c26898229a25060b081e4
JK <61711289+jklick-so@users.noreply.github.com>
2023-12-12 15:46:29 -0500

    v1.0 release

---
+++ README.md
@@ -0,0 +1,56 @@
+# Stack Overflow for Teams Inactive Users (so4t_inactive_users)
+An Python script that uses the Stack Overflow for Teams API to create a CSV report of inactive users. You can see an example of what the output looks like in the Examples directory ([here](https://github.com/jklick-so/so4t_inactive_users/blob/main/Examples/inactive_users.csv)).
+
+
+## Table of Contents
+* [Requirements](https://github.com/jklick-so/so4t_inactive_users#requirements)
+* [Setup](https://github.com/jklick-so/so4t_inactive_users#setup)
+* [Usage](https://github.com/jklick-so/so4t_inactive_users#basic-usage)
+* [Support, security, and legal](https://github.com/jklick-so/so4t_inactive_users#support-security-and-legal)
+
+
+## Requirements
+* A Stack Overflow for Teams instance (Basic, Business, or Enterprise)
+* Python 3.8 or higher ([download](https://www.python.org/downloads/))
+* Operating system: Linux, MacOS, or Windows
+
+
+## Setup
+
+[Download](https://github.com/jklick-so/so4t_inactive_users/archive/refs/heads/main.zip) and unpack the contents of this repository
+
+**Installing Dependencies**
+
+* Open a terminal window (or, for Windows, a command prompt)
+* Navigate to the directory where you unpacked the files
+* Install the dependencies: `pip3 install -r requirements.txt`
+
+**API Authentication**
+
+For the Basic and Business tiers, you'll need an API token. For Enterprise, you'll need to obtain an API key.
+
+* For Basic or Business, instructions for creating a personal access token (PAT) can be found in [this KB article](https://stackoverflow.help/en/articles/4385859-stack-overflow-for-teams-api).
+* For Enterprise, documentation for creating an API key can be found within your instance, at this url: `https://[your_site]/api/docs/authentication`
+
+
+## Usage
+
+In a terminal window, navigate to the directory where you unpacked the script. 
+Run the script using the following format, replacing the URL, token, and/or key with your own:
+* For Basic and Business: `python3 so4t_inactive_users.py --url "https://stackoverflowteams.com/c/TEAM-NAME" --token "YOUR_TOKEN"`
+* For Enterprise: `python3 so4t_inactive_users.py --url "https://SUBDOMAIN.stackenterprise.co" --key "YOUR_KEY"`
+
+As the script runs, it will continue to update the terminal window with the tasks it's performing. When the script completes, it will indicate the reports have been generated, along with the name of file. 
+
+Three reports are generated:
+* `all_users_inactive_for_##_days.csv` - as the name implies, this is all users who have not logged in within the specified number of days
+* `contributing_users_inactive_for_##_days.csv` - this is a subset of the `all_users` report, and includes only users who have contributed content. The use case for this report is identifying a subset of inactive users who might be adversely impacted if their account was deleted (i.e. they'd lose their user profile, reputation gains, content attribution, etc.)
+* `noncontributing_users_inactive_for_##_days.csv` - this is a subset of the `all_users` report, including only users who have *not* contributed any content; in other words, the delta between the `all_users` and `contributing_users` reports. It's likely that these users can be safely deleted; and, if they're deleted prematurely (or in error), they can simply register for a new account and create a new user profile without experiencing any loss of reputation points, contribut attribution, etc.
+
+
+## Support, security, and legal
+Disclaimer: the creator of this project works at Stack Overflow, but it is a labor of love that comes with no formal support from Stack Overflow. 
+
+If you run into issues using the script, please [open an issue](https://github.com/jklick-so/so4t_tag_report/issues). You are also welcome to edit the script to suit your needs, steal the code, or do whatever you want with it. It is provided as-is, with no warranty or guarantee of any kind. If the creator wasn't so lazy, there would likely be an MIT license file included.
+
+All data is handled locally on the device from which the script is run. The script does not transmit data to other parties, such as Stack Overflow. All of the API calls performed are read only, so there is no risk of editing or adding content on your Stack Overflow for Teams instance.
---
+++ so4t_inactive_users.py
@@ -0,0 +1,167 @@
+'''
+This Python script is a labor of love and has no formal support from Stack Overflow. 
+If you run into difficulties, reach out to the person who provided you with this script.
+Or, open an issue here: https://github.com/jklick-so/so4t_inactive_users/issues
+'''
+
+# Standard Python libraries
+import argparse
+import csv
+import time
+
+# Local libraries
+from so4t_api_v2 import V2Client
+
+
+def main():
+
+    # Get command-line arguments
+    args = get_args()
+    if args.days: # Number of days of inactivity to consider a user inactive
+        inactive_days = args.days
+    else:
+        inactive_days = 180 # Default to 180 days
+
+    # Create a client object
+    client = V2Client(args.url, key=args.key, token=args.token)
+
+    # Get user data
+    users = get_user_data(client)
+
+    # Create user lists
+    current_unix_time = int(time.time())
+    inactive_threshold = current_unix_time - (inactive_days * 86400)
+
+    all_inactive_users = []
+    inactive_users_without_contributions = []
+    inactive_users_with_contributions = []
+    for user in users:
+        if user['last_access_date'] < inactive_threshold:
+            user['inactive_days'] = round((current_unix_time - user['last_access_date']) / 86400)
+            all_inactive_users.append(user)
+            if not (user['answer_count'] > 0 or user['question_count'] > 0 or user['article_count'] > 0 
+                or user['comment_count'] > 0):
+                inactive_users_without_contributions.append(user)
+            else:
+                inactive_users_with_contributions.append(user)
+
+    # Write user data to CSV
+    write_user_data(f'all_users_inactive_for_{inactive_days}_days.csv', all_inactive_users)
+    write_user_data(f'noncontributing_users_inactive_for_{inactive_days}_days.csv', 
+        inactive_users_without_contributions)
+    write_user_data(f'contributing_users_inactive_for_{inactive_days}_days.csv',
+        inactive_users_with_contributions)
+
+
+def get_args():
+
+    parser = argparse.ArgumentParser(
+        prog='so4t_inactive_users.py',
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        description='Uses the Stack Overflow for Teams API to create \
+        a user report.',
+        epilog = 'Example for Stack Overflow Business: \n'
+                'python3 so4t_inactive_users.py --url "https://stackoverflowteams.com/c/TEAM-NAME" '
+                '--token "YOUR_TOKEN" \n\n'
+                'Example for Stack Overflow Enterprise: \n'
+                'python3 so4t_inactive_users.py --url "https://SUBDOMAIN.stackenterprise.co" '
+                '--key "YOUR_KEY"\n\n')
+    
+    parser.add_argument('--url', 
+                        type=str,
+                        help='Base URL for your Stack Overflow for Teams instance. '
+                        'Required if --no-api is not used')
+    parser.add_argument('--token',
+                        type=str,
+                        help='API token for your Stack Overflow for Teams instance. '
+                        'Required if --no-api is not used')
+    parser.add_argument('--key',
+                    type=str,
+                    help='API key value. Required if using Enterprise and --no-api is not used')
+    parser.add_argument('--days',
+                        type=int,
+                        help='Only include metrics for content created within the past X days. '
+                        'Default is to include all history')
+
+    return parser.parse_args()
+
+
+
+def get_user_data(client):
+
+    if client.soe: # Stack Overflow Enterprise
+        filter_attributes = [
+            'user.answer_count',
+            'user.down_vote_count',
+            'user.is_deactivated',
+            'user.question_count',
+            'user.up_vote_count',
+            'user.verified_email'
+        ]
+        filter_string = client.create_filter(filter_attributes)
+    else: # Stack Overflow Business
+        filter_string = '!6WPIommaBqvw3'
+
+    users = client.get_all_users(filter_string)
+
+    # Filter out users with a user_id less than `1`
+    users = [user for user in users if user['user_id'] > 0]
+
+    # Add additional user fields
+    for user in users:
+        user['article_count'] = 0
+        user['comment_count'] = 0
+
+    # Get additional user data from comments and articles
+    articles = client.get_all_articles()
+    comments = client.get_all_comments()
+
+    for article in articles:
+        try:
+            article_owner_id = article['owner']['user_id']
+        except KeyError:
+            continue
+        for user in users:
+            if user['user_id'] == article_owner_id:
+                user['article_count'] += 1
+    
+    for comment in comments:
+        try:
+            comment_owner_id = comment['owner']['user_id']
+        except KeyError:
+            continue
+        for user in users:
+            if user['user_id'] == comment_owner_id:
+                user['comment_count'] += 1
+
+    return users
+
+
+def write_user_data(file_name, users):
+
+    with open(file_name, 'w', newline='') as csv_file:
+        field_names = [
+            'user_id',
+            'verified_email',
+            'display_name',
+            'inactive_days',
+            'is_deactivated',
+            'reputation',
+            'answer_count',
+            'question_count',
+            'article_count',
+            'comment_count',
+            'down_vote_count',
+            'up_vote_count'
+        ]
+        writer = csv.DictWriter(csv_file, fieldnames=field_names, extrasaction='ignore')
+        writer.writeheader()
+        for user in users:
+            writer.writerow(user)
+
+        print(f"User data written to {file_name}")
+
+
+if __name__ == '__main__':
+
+    main()
